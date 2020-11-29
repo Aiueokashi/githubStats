bca14709d4a0369d6dc7abee057c669e999ff2a5
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2020-11-29 12:50:08 +0900

    Update stats-card.js

--- src/cards/stats-card.js
+++ src/cards/stats-card.js
@@ -89,7 +89,7 @@ const renderStatsCard = (stats = {}, options = { hide: [] }) => {
   // Meta data for creating text nodes with createTextNode function
   const STATS = {
     stars: {
-      icon: icons.star,
+      icon: "⭐",
       label: i18n.t("statcard.totalstars"),
       value: totalStars,
       id: "stars",
